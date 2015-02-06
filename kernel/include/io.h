48e33bd3ee5f62f1b27d8a7aa6cfb697b497e03f
Franklin Wei <frankhwei536@gmail.com>
2015-02-05 20:57:30 -0500

    add outw function

--- kernel/include/io.h
+++ kernel/include/io.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
 void outb(uint16_t port, uint8_t val);
+void outw(uint16_t port, uint16_t val);
 uint8_t inb(uint16_t port);
 uint16_t inw(uint16_t port);
--- kernel/io.c
+++ kernel/io.c
@@ -5,12 +5,19 @@ void outb(uint16_t port, uint8_t val)
 {
     asm volatile ("outb %1, %0": :"dN" (port), "a" (val));
 }
+
+void outw(uint16_t port, uint16_t val)
+{
+    asm volatile ("outw %1, %0": :"dN" (port), "a"(val));
+}
+
 uint8_t inb(uint16_t port)
 {
     uint8_t ret;
     asm volatile ("inb %1, %0" : "=a" (ret) : "dN" (port));
     return ret;
 }
+
 uint16_t inw(uint16_t port)
 {
     uint16_t ret;
