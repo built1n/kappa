3fbc39f482df596c832cf4ffcf3956a20e71cb38
Franklin Wei <frankhwei536@gmail.com>
2015-02-18 12:55:32 -0500

    bump version

--- kernel/include/version.h
+++ kernel/include/version.h
@@ -1,2 +1,2 @@
-#define KAPPA_KERNEL_VERSION "0.0.0-alpha"
-#define KAPPA_KERNEL_CODENAME "Ayatollah's Aqueduct"
+#define KAPPA_KERNEL_VERSION "0.0.1-alpha"
+#define KAPPA_KERNEL_CODENAME "Ayatollah's Buybacks"
